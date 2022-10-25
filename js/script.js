f51e8ee50ad0d63f3602051f733814a7c7c6e5e5
IlCarretto <carlorebuffat412@gmail.com>
2022-10-25 16:41:15 +0200

    done with features

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
+// Pari dispari:
+// 1. predisporre l'input dell'utente per chiedere un numero
+// 2. al click sul bottone verificare se il numero è pari o dispari, facendo vedere il risultato in altert
+// 3. Stampare il risultato in pagina
+// 4. Colorare il testo stampato diversamente se pari o dispari
+
 const {createApp} = Vue;
 
 createApp ({
     data() {
         return {
-            title: "Sono un titolo",
+            userNum: "",
+            numResult: "",
+            titleNum: "",
             img: ("https://picsum.photos/200/300")
         }
     },
     methods: {
-
+        clickButton: function() {
+            if (this.userNum % 2 === 0) {
+                this.numResult = "pari";
+            } else {
+                this.numResult = "dispari"
+            }
+            this.titleNum = `Ciao! il tuo numero è ${this.numResult}!`
+        }
     }
 }).mount("#app");
