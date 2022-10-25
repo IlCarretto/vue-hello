038cbecc1a7b6aeb6bfbd9b1cf4aa947c0c64cb3
IlCarretto <carlorebuffat412@gmail.com>
2022-10-25 13:24:15 +0200

    done with bonus

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const {createApp} = Vue;
 createApp ({
     data() {
         return {
-
+            title: "Sono un titolo",
+            img: ("https://picsum.photos/200/300")
         }
     },
     methods: {
